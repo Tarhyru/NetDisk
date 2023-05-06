225f62d190bb16d67f56244dc545b1bce2c5dca2
Zephyru <zephyru@163.com>
2023-05-06 14:51:59 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,25 +2,47 @@ var script = document.createElement('script')
 script.type = 'text/javascript'
 script.src = 'https://cdn.jsdelivr.net/npm/echarts/dist/echarts.min.js'
 document.head.appendChild(script)
+var isInit = false
+
 script.onload = function () {
-  var dom = $('[data-label=test]').get(0)
-  var myChart = echarts.init(dom)
+	console.log('check script onload')
+	init();
+}
 
-  var option = {
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-      type: 'value'
-    },
-    series: [
-      {
-        data: [150, 230, 224, 218, 135, 147, 260],
-        type: 'line'
-      }
-    ]
-  }
+setTimeout(() => {
+	init();
+}, 800)
 
-  myChart.setOption(option, true)
+function init() {
+	if (isInit) {
+		return
+	}
+	var doms = document.querySelectorAll('[data-label="test"]')
+	console.log('check doms', doms)
+	if (doms && doms.length > 0) {
+		if (echarts) {
+			isInit = true
+			var dom = doms[0]
+			var myChart = echarts.init(dom)
+
+			var option = {
+				xAxis: {
+					type: 'category',
+					data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+				},
+				yAxis: {
+					type: 'value'
+				},
+				series: [
+					{
+						data: [150, 230, 224, 218, 135, 147, 260],
+						type: 'line'
+					}
+				]
+			}
+
+			myChart.setOption(option, true)
+		}
+
+	}
 }
